dbde74f78db18c4f2660cb9aeb9b965cdb87d25d
mrAbra <35962429+mrAbra@users.noreply.github.com>
2019-04-24 22:11:14 +0300

    vivado

---
+++ project_1/project_1.srcs/sources_1/new/wcdma_ovsf_generator.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: orlik.a.a.
+// 
+// Create Date: 24.04.2019 21:33:44
+// Design Name: 
+// Module Name: wcdma_ovsf_generator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wcdma_ovsf_generator(
+    input ovsf_num,
+    input sf
+    );
+    
+   
+localparam ololo = 3;
+    
+endmodule
